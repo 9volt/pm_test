040017a0df8f8b0224205ad458dd9843aaf0c28c
Joseph Utecht <thenmal@gmail.com>
2014-05-10 13:18:48 -0500

    initial commit

---
+++ Assets/hit_test.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hit_test : MonoBehaviour {
+	private Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1")){
+			Debug.Log("hit");
+			anim.SetTrigger("hit");
+		}
+		if(Input.GetButtonDown("Fire2")){
+			anim.SetBool("alive", !anim.GetBool("alive"));
+		}
+		if(Input.GetButtonDown("Fire3")){
+			anim.SetTrigger("swing");
+		}
+	}
+}
