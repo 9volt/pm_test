5d5be47159150839f5ad46d52c1fbf1b7f656853
Joseph Utecht <thenmal@gmail.com>
2014-06-16 22:45:07 -0500

    added custom actions for party and enemy selection

---
+++ Assets/battle_scripts/BattleState.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Enemy {
+	public int health;
+	public string name;
+
+	public Enemy(int h, string n){
+		health = h;
+		name = n;
+	}
+}
+
+public class Ally {
+	public int health;
+	public string name;
+
+	public Ally(int h, string n){
+		health = h;
+		name = n;
+	}
+}
+
+public class BattleState : MonoBehaviour {
+	public List<Enemy> enemies;
+	public List<Ally> allies;
+
+	// Use this for initialization
+	void Start () {
+		enemies = new List<Enemy>();
+		allies = new List<Ally>();
+
+		enemies.Add(new Enemy(5, "Bad Guy"));
+		allies.Add(new Ally(5, "Good Guy"));
+		allies.Add(new Ally(5, "Good Guy2"));
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/battle_scripts/PartySelect.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace HutongGames.PlayMaker.Actions {
+	[ActionCategory(ActionCategory.GUI)]
+	[Tooltip("Select your party member.")]
+	public class PartySelect : GUIContentAction {
+		public FsmEvent sendEvent;
+		private BattleState bs;
+
+		public override void OnEnter(){
+			bs = GameObject.FindGameObjectWithTag("battlestate").GetComponent<BattleState>();
+		}
+
+		// Update is called once per frame
+		public override void Reset () {
+			bs = GameObject.FindGameObjectWithTag("battlestate").GetComponent<BattleState>();
+		}
+
+		public override void OnGUI(){
+			base.OnGUI();
+
+			float top = .75f * Screen.height;
+			float left = .25f * Screen.width;
+			float width = .2f * Screen.width;
+			float height = .1f * Screen.height;
+			foreach(Ally a in bs.allies){
+				if(GUI.Button(new Rect(left, top, width, height), a.name)){
+					Fsm.Event(sendEvent);
+				}
+				top = top + height;
+			}
+		}
+	}
+}
